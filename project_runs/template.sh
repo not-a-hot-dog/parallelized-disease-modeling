1f1fb015ac3b274d1b276cb503241cfe77bafc43
CoopLo <cooper.lorsung@gmail.com>
2020-05-10 22:55:44 -0400

    Fixed path to be consistent with other scripts

--- project_runs/template.sh
+++ project_runs/template.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-#SBATCH -n NC # Number of cores
-#SBATCH -N NN # Ensure that all cores are on one machine
+#SBATCH -J slurm_NC
+#SBATCH --account=cs205
+#SBATCH --cpus-per-task NC
+#SBATCH -N 1 # Ensure that all cores are on one machine
 #SBATCH -t TIME # Runtime in D-HH:MM
 #SBATCH -p PARTITION # partition
-#SBATCH --mem=MEM # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem-per-cpu=MEM # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o OUTPUTDIR/out_%j.txt # File to which STDOUT will be written
 #SBATCH -e OUTPUTDIR/err_%j.txt # File to which STDERR will be written
-
-srun ./sir_omp KWARGS > OUTPUTDIR/output
+export SLURM_CPUS_PER_TASK=NC
+export OMP_NUM_THREADS=NC
+srun sir STEPS OUTPUTDIR/output > OUTPUTDIR/results.txt
